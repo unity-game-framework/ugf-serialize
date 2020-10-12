5ef4a96c3341c0c254ac91e86d18cfc8e984430e
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-10-12 19:49:14 +0300

    Update SerializerFormatter.cs

--- Packages/UGF.Serialize/Runtime/Formatter/SerializerFormatter.cs
+++ Packages/UGF.Serialize/Runtime/Formatter/SerializerFormatter.cs
@@ -1,13 +1,14 @@
 using System;
 using System.IO;
 using System.Runtime.Serialization;
+using System.Threading.Tasks;
 
 namespace UGF.Serialize.Runtime.Formatter
 {
     /// <summary>
     /// Represents serialized that use formatter to serialize a specified target to byte array and vice versa.
     /// </summary>
-    public class SerializerFormatter : SerializerBase<byte[]>
+    public class SerializerFormatter : SerializerAsyncBase<byte[]>
     {
         /// <summary>
         /// Gets the formatter used to convert target to byte array and vice versa.
@@ -25,25 +26,44 @@ namespace UGF.Serialize.Runtime.Formatter
 
         public override byte[] Serialize(object target)
         {
-            if (target == null) throw new ArgumentNullException(nameof(target));
-
-            using (var stream = new MemoryStream())
-            {
-                Formatter.Serialize(stream, target);
-
-                return stream.ToArray();
-            }
+            return SerializeInternal(Formatter, target);
         }
 
         public override object Deserialize(Type targetType, byte[] data)
         {
-            if (targetType == null) throw new ArgumentNullException(nameof(targetType));
+            return DeserializeInternal(Formatter, data);
+        }
+
+        public override Task<byte[]> SerializeAsync(object target)
+        {
+            return Task.Run(() => SerializeInternal(Formatter, target));
+        }
+
+        public override Task<object> DeserializeAsync(Type targetType, byte[] data)
+        {
+            return Task.Run(() => DeserializeInternal(Formatter, data));
+        }
+
+        private static byte[] SerializeInternal(IFormatter formatter, object target)
+        {
+            if (formatter == null) throw new ArgumentNullException(nameof(formatter));
+            if (target == null) throw new ArgumentNullException(nameof(target));
+
+            using var stream = new MemoryStream();
+
+            formatter.Serialize(stream, target);
+
+            return stream.ToArray();
+        }
+
+        private static object DeserializeInternal(IFormatter formatter, byte[] data)
+        {
+            if (formatter == null) throw new ArgumentNullException(nameof(formatter));
             if (data == null) throw new ArgumentNullException(nameof(data));
 
-            using (var stream = new MemoryStream(data))
-            {
-                return Formatter.Deserialize(stream);
-            }
+            using var stream = new MemoryStream(data);
+
+            return formatter.Deserialize(stream);
         }
     }
 }
