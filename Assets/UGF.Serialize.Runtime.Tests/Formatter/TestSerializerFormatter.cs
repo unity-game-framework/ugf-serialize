85ff61d0be6852a327b8b8f2c19b34cbbb6f8d50
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-10-12 19:54:37 +0300

    Update TestSerializerFormatter

--- Assets/UGF.Serialize.Runtime.Tests/Formatter/TestSerializerFormatter.cs
+++ Assets/UGF.Serialize.Runtime.Tests/Formatter/TestSerializerFormatter.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Collections;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading.Tasks;
 using NUnit.Framework;
 using UGF.Serialize.Runtime.Formatter;
+using UnityEngine.TestTools;
 
 namespace UGF.Serialize.Runtime.Tests.Formatter
 {
@@ -35,6 +38,47 @@ namespace UGF.Serialize.Runtime.Tests.Formatter
 
             byte[] bytes = serializer.Serialize(target);
             var target0 = serializer.Deserialize<Target>(bytes);
+
+            Assert.AreEqual(target.BoolValue, target0.BoolValue);
+            Assert.AreEqual(target.IntValue, target0.IntValue);
+            Assert.AreEqual(target.FloatValue, target0.FloatValue);
+        }
+
+        [UnityTest]
+        public IEnumerator SerializeAsync()
+        {
+            var serializer = new SerializerFormatter(new BinaryFormatter());
+            var target = new Target();
+
+            Task<byte[]> task = serializer.SerializeAsync(target);
+
+            while (!task.IsCompleted)
+            {
+                yield return null;
+            }
+
+            byte[] bytes = task.Result;
+
+            Assert.NotNull(bytes);
+            Assert.Greater(bytes.Length, 0);
+        }
+
+        [UnityTest]
+        public IEnumerator DeserializeAsync()
+        {
+            var serializer = new SerializerFormatter(new BinaryFormatter());
+            var target = new Target();
+
+            byte[] bytes = serializer.Serialize(target);
+
+            Task<Target> task = serializer.DeserializeAsync<Target>(bytes);
+
+            while (!task.IsCompleted)
+            {
+                yield return null;
+            }
+
+            Target target0 = task.Result;
 
             Assert.AreEqual(target.BoolValue, target0.BoolValue);
             Assert.AreEqual(target.IntValue, target0.IntValue);
