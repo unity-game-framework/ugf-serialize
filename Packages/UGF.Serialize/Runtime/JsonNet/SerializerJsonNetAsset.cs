dd7482e56bd8003466dfebbd461f485f7cc87fe5
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-10-18 18:39:26 +0300

    Add SerializerJsonNetAsset

---
+++ Packages/UGF.Serialize/Runtime/JsonNet/SerializerJsonNetAsset.cs
@@ -0,0 +1,24 @@
+﻿#if UGF_SERIALIZE_JSONNET
+using UnityEngine;
+
+namespace UGF.Serialize.Runtime.JsonNet
+{
+    [CreateAssetMenu(menuName = "UGF/Serialize/Serializer JsonNet", order = 2000)]
+    public class SerializerJsonNetAsset : SerializerAsset<string>
+    {
+        [SerializeField] private bool m_readable;
+
+        public bool Readable { get { return m_readable; } set { m_readable = value; } }
+
+        protected override ISerializer<string> OnBuildTyped()
+        {
+            return new SerializerJsonNet(m_readable);
+        }
+
+        private void Reset()
+        {
+            Name = "jsonnet-text";
+        }
+    }
+}
+#endif
