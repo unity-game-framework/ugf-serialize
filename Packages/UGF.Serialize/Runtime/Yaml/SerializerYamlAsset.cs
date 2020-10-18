aca5b863a7d96752d1e70719ba3be7fab29ed97b
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-10-18 18:45:41 +0300

    Add SerializerYamlAsset

---
+++ Packages/UGF.Serialize/Runtime/Yaml/SerializerYamlAsset.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace UGF.Serialize.Runtime.Yaml
+{
+    [CreateAssetMenu(menuName = "UGF/Serialize/Serializer Yaml", order = 2000)]
+    public class SerializerYamlAsset : SerializerAsset<string>
+    {
+        protected override ISerializer<string> OnBuildTyped()
+        {
+            return new SerializerYaml();
+        }
+
+        private void Reset()
+        {
+            Name = "yaml-text";
+        }
+    }
+}
