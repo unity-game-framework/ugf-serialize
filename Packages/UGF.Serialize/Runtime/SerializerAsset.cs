3ceaeb252426974ee82dacba7f38b8b883d7b2a2
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-12-03 20:30:01 +0300

    Change builder

--- Packages/UGF.Serialize/Runtime/SerializerAsset.cs
+++ Packages/UGF.Serialize/Runtime/SerializerAsset.cs
@@ -1,26 +1,15 @@
 ﻿using System;
+using UGF.Builder.Runtime;
 using UnityEngine;
 
 namespace UGF.Serialize.Runtime
 {
-    public abstract class SerializerAsset : ScriptableObject, ISerializerBuilder
+    public abstract class SerializerAsset : BuilderAsset<ISerializer>, ISerializerBuilder
     {
         [SerializeField] private string m_name;
 
         public string Name { get { return m_name; } set { m_name = value; } }
 
         public abstract Type DataType { get; }
-
-        public T Build<T>() where T : ISerializer
-        {
-            return (T)OnBuild();
-        }
-
-        public ISerializer Build()
-        {
-            return OnBuild();
-        }
-
-        protected abstract ISerializer OnBuild();
     }
 }
