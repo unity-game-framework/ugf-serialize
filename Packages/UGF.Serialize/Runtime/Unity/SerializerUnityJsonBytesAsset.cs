4e9a75ce5a251701fedd07026751f1211ffcbdbb
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-07-22 15:25:49 +0300

    Add encoding selection

--- Packages/UGF.Serialize/Runtime/Unity/SerializerUnityJsonBytesAsset.cs
+++ Packages/UGF.Serialize/Runtime/Unity/SerializerUnityJsonBytesAsset.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using UGF.RuntimeTools.Runtime.Encodings;
 using UnityEngine;
 
 namespace UGF.Serialize.Runtime.Unity
@@ -6,16 +7,15 @@ namespace UGF.Serialize.Runtime.Unity
     [CreateAssetMenu(menuName = "Unity Game Framework/Serialize/Serializer Unity Json Bytes", order = 2000)]
     public class SerializerUnityJsonBytesAsset : SerializerAsset<byte[]>
     {
+        [SerializeField] private EncodingType m_encoding;
+
+        public EncodingType Encoding { get { return m_encoding; } set { m_encoding = value; } }
+
         protected override ISerializer<byte[]> OnBuildTyped()
         {
-            Encoding encoding = OnCreateEncoding();
+            Encoding encoding = EncodingUtility.GetEncoding(m_encoding);
 
             return new SerializerUnityJsonBytes(encoding);
-        }
-
-        protected virtual Encoding OnCreateEncoding()
-        {
-            return Encoding.Default;
         }
     }
 }
