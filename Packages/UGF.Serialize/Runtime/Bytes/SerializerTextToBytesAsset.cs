269148af204af7b210273c803b60c7ea1b227e9a
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-07-22 15:00:35 +0300

    Add encoding selection for SerializerUnityJsonBytesAsset and SerializerTextToBytesAsset

--- Packages/UGF.Serialize/Runtime/Bytes/SerializerTextToBytesAsset.cs
+++ Packages/UGF.Serialize/Runtime/Bytes/SerializerTextToBytesAsset.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Text;
+using UGF.RuntimeTools.Runtime.Encodings;
+using UnityEngine;
 
 namespace UGF.Serialize.Runtime.Bytes
 {
@@ -6,14 +8,17 @@ namespace UGF.Serialize.Runtime.Bytes
     public class SerializerTextToBytesAsset : SerializerAsset<byte[]>
     {
         [SerializeField] private SerializerAsset m_text;
+        [SerializeField] private EncodingType m_encoding;
 
         public SerializerAsset Text { get { return m_text; } set { m_text = value; } }
+        public EncodingType Encoding { get { return m_encoding; } set { m_encoding = value; } }
 
         protected override ISerializer<byte[]> OnBuildTyped()
         {
             var serializer = m_text.Build<ISerializer<string>>();
+            Encoding encoding = EncodingUtility.GetEncoding(m_encoding);
 
-            return new SerializerTextToBytes(serializer);
+            return new SerializerTextToBytes(encoding, serializer);
         }
     }
 }
