277cb52406befd5e29527a2a83481aa05b83b879
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-11-23 21:57:48 +0300

    Add SerializerContainer

---
+++ Packages/UGF.Serialize/Runtime/Container/SerializerContainer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using UGF.RuntimeTools.Runtime.Contexts;
+
+namespace UGF.Serialize.Runtime.Container
+{
+    public abstract class SerializerContainer<TContainer, TTarget, TData> : SerializerAsync<TData> where TContainer : class, new()
+    {
+        public ISerializerAsync<TData> ContainerSerializer { get; }
+
+        protected SerializerContainer(ISerializerAsync<TData> containerSerializer)
+        {
+            ContainerSerializer = containerSerializer ?? throw new ArgumentNullException(nameof(containerSerializer));
+        }
+
+        protected override object OnSerialize(object target, IContext context)
+        {
+            TContainer container = OnCreateContainer((TTarget)target, context);
+            TData data = ContainerSerializer.Serialize(container, context);
+
+            return data;
+        }
+
+        protected override object OnDeserialize(Type targetType, TData data, IContext context)
+        {
+            var container = ContainerSerializer.Deserialize<TContainer>(data, context);
+            TTarget target = OnCreateTarget(container, context);
+
+            return target;
+        }
+
+        protected override async Task<TData> OnSerializeAsync(object target, IContext context)
+        {
+            TContainer container = OnCreateContainer((TTarget)target, context);
+            TData data = await ContainerSerializer.SerializeAsync(container, context);
+
+            return data;
+        }
+
+        protected override async Task<object> OnDeserializeAsync(Type targetType, TData data, IContext context)
+        {
+            var container = await ContainerSerializer.DeserializeAsync<TContainer>(data, context);
+            TTarget target = OnCreateTarget(container, context);
+
+            return target;
+        }
+
+        protected abstract TContainer OnCreateContainer(TTarget target, IContext context);
+        protected abstract TTarget OnCreateTarget(TContainer container, IContext context);
+    }
+}
---
+++ Packages/UGF.Serialize/Runtime/Container/SerializerContainerAsset.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace UGF.Serialize.Runtime.Container
+{
+    public abstract class SerializerContainerAsset<TData> : SerializerAsset<TData>
+    {
+        [SerializeField] private SerializerAsset m_container;
+
+        public SerializerAsset Container { get { return m_container; } set { m_container = value; } }
+    }
+}
