1e377bd951bdd81097542583781f74f260e8e4ac
Vorobey <vorobyovvorobyov@gmail.com>
2022-09-18 19:56:41 +0300

    Add SerializeEditorUtility

---
+++ Packages/UGF.Serialize/Editor/SerializeEditorUtility.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using UGF.Serialize.Runtime;
+using UnityEditor;
+
+namespace UGF.Serialize.Editor
+{
+    public static class SerializeEditorUtility
+    {
+        public static void GetSerializeTypes(IDictionary<object, Type> types)
+        {
+            if (types == null) throw new ArgumentNullException(nameof(types));
+
+            foreach ((Type type, SerializeTypeAttribute attribute) in GetSerializeTypes())
+            {
+                if (attribute.HasId)
+                {
+                    types.Add(attribute.Id, type);
+                }
+            }
+        }
+
+        public static IEnumerable<(Type type, SerializeTypeAttribute Attribute)> GetSerializeTypes()
+        {
+            TypeCache.TypeCollection collection = TypeCache.GetTypesWithAttribute<SerializeTypeAttribute>();
+
+            foreach (Type type in collection)
+            {
+                var attribute = type.GetCustomAttribute<SerializeTypeAttribute>();
+
+                yield return (type, attribute);
+            }
+        }
+
+        public static bool IsValidSerializableType(Type type)
+        {
+            if (type == null) throw new ArgumentNullException(nameof(type));
+
+            return !type.IsAbstract
+                   && !type.IsArray
+                   && !type.IsEnum
+                   && !type.IsInterface
+                   && !type.IsGenericTypeDefinition
+                   && !type.IsGenericType
+                   && type.GetConstructor(Type.EmptyTypes) != null
+                   && type.GetCustomAttribute<CompilerGeneratedAttribute>() == null;
+        }
+    }
+}
